6f54c49f99c80c25a68a387d356cae5c182b2458
kubi <pietrusjakub@gmail.com>
2020-04-02 01:14:26 +0200

    Carlier-update

---
+++ lab3-carlier/Carlier/MinPriorityQueue.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace Carlier
+{
+    public class MinPriorityQueue
+    {
+        public MinPriorityQueue(Task[] tasks)
+        {
+            foreach (var task in tasks)
+            {
+                Add(task);
+            }
+        }
+
+        private static int capacity = 100;
+        private int size = 0;
+
+        Task[] items = new Task[capacity];
+
+
+        private int getLeftChildIndex(int parentindex) { return 2 * parentindex + 1; }
+        private int getRightChildIndex(int parentindex) { return 2 * parentindex + 2; }
+        private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
+
+        private bool HasLeftChild(int index) { return getLeftChildIndex(index) < size; }
+        private bool HasRightChild(int index) { return getRightChildIndex(index) < size; }
+        private bool HasParent(int index) { return getParentIndex(index) >= 0; }
+
+        private Task LeftChild(int index) { return items[getLeftChildIndex(index)]; }
+        private Task RightChild(int index) { return items[getRightChildIndex(index)]; }
+        private Task Parent(int index) { return items[getParentIndex(index)]; }
+
+
+        private void Swap(int indexOne, int indexTwo)
+        {
+            var temp = items[indexOne];
+            items[indexOne] = items[indexTwo];
+            items[indexTwo] = temp;
+        }
+
+        private void RemoveAt(int index)
+        {
+            if (size == 0) Console.WriteLine("MinQueue: RemoveAt: Pusta kolejka");
+
+            var item = items[index];
+            Swap(index, size - 1);
+            size--;
+
+            var ele = items[index];
+
+            HeapifyDown();
+
+            if (items[index].Equals(ele))
+                HeapifyUp();
+        }
+
+        private void EnsureExtraCapacity()
+        {
+            if (size == capacity)
+            {
+                Array.Resize(ref items, capacity * 2);
+                capacity *= 2;
+            }
+        }
+
+        private void HeapifyUp()
+        {
+            int index = size - 1;
+            
+            while (HasParent(index) && Parent(index).r > items[index].r)
+            {
+                Swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }
+        }
+
+        private void HeapifyDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                int smallerChildIndex = getLeftChildIndex(index);
+                if (HasRightChild(index) && RightChild((index)).r < LeftChild(index).r)
+                {
+                    smallerChildIndex = getRightChildIndex(index);
+                }
+
+                if (items[index].r < items[smallerChildIndex].r)
+                {
+                    break;
+                }
+                else
+                {
+                    Swap(index, smallerChildIndex);
+                }
+                index = smallerChildIndex;
+
+            }
+        }
+
+        /// <summary>
+        /// Returns true if is empty, otherwise false
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            if (size == 0)
+                return true;
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// Returns min element but doesn't delete them
+        /// </summary>
+        /// <returns></returns>
+        public Task Peek()
+        {
+            if (size == 0)
+                if (size == 0) Console.WriteLine("MinQueue: Peek: Pusta kolejka");
+
+            return items[0];
+        }
+
+        /// <summary>
+        /// Return min element, delete it, and heapify
+        /// </summary>
+        /// <returns></returns>
+        public Task Poll()
+        {
+            var item = items[0];
+            RemoveAt(0);
+            return item;
+        }
+
+        /// <summary>
+        /// Add element to heap, and heapify
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(Task item)
+        {
+            EnsureExtraCapacity();
+            items[size] = item;
+            size++;
+            HeapifyUp();
+        }
+
+        /// <summary>
+        /// Remove item with current value, and heapify
+        /// </summary>
+        /// <param name="item"></param>
+        public bool Remove(Task element)
+        {
+
+            for (int i = 0; i < size; i++)
+            {
+                if (element.Equals(items[i]))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
---
+++ lab3-carlier/Carlier/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace Carlier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpq = new RPQ(3);
+            var pi = new Task[rpq.n];
+            var tasks = rpq.tasks;
+            rpq.Schrage(pi);
+            
+            var b = rpq.GetB(pi);
+            var a = rpq.GetA(pi,b);
+            var c = rpq.GetC(pi,a, b);
+            var k = rpq.GetK(pi, b, c);
+
+
+
+            //Console.WriteLine("B:(index):  " + b);
+            //Console.WriteLine("B:(wartość d):  " + tasks[b].d);
+            //Console.WriteLine("B:(wartość c):  " + tasks[b].c);
+            //Console.WriteLine("B:(wartość r):  " + tasks[b].r);
+            //Console.WriteLine("B:(wartość p):  " + tasks[b].p);
+            //Console.WriteLine("B:(wartość q):  " + tasks[b].q);
+
+            //Console.WriteLine();
+            //Console.WriteLine("A:(index):  " + a);
+            //Console.WriteLine("A:(wartość d):  " + tasks[a].d);
+            //Console.WriteLine("A:(wartość c):  " + tasks[a].c);
+            //Console.WriteLine("A:(wartość r):  " + tasks[a].r);
+            //Console.WriteLine("A:(wartość p):  " + tasks[a].p);
+            //Console.WriteLine("A:(wartość q):  " + tasks[a].q);
+
+            //Console.WriteLine();
+            //Console.WriteLine("C:(index):  " + c);
+            //Console.WriteLine("C:(wartość d):  " + tasks[c].d);
+            //Console.WriteLine("C:(wartość c):  " + tasks[c].c);
+            //Console.WriteLine("C:(wartość r):  " + tasks[c].r);
+            //Console.WriteLine("C:(wartość p):  " + tasks[c].p);
+            //Console.WriteLine("C:(wartość q):  " + tasks[c].q);
+
+            //Console.WriteLine("hehe");
+            //Console.WriteLine("hehe");
+            //Console.WriteLine("hehe");
+
+            //foreach (var item in k)
+            //{
+            //    Console.WriteLine(item.d);
+            //}
+
+            //var xd = rpq.Carlier();
+
+            //Console.WriteLine(xd.Max(x=>x.d));
+
+            Test.Run();
+
+
+        }
+    }
+}
