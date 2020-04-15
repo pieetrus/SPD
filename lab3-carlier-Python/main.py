b07576759461112fca63187b28acfd0916a1ac5b
kubi <pietrusjakub@gmail.com>
2020-04-15 21:57:45 +0200

    XD

--- lab3-carlier-Python/main.py
+++ lab3-carlier-Python/main.py
@@ -1,79 +1,134 @@
-'''[r,p,q]'''
-from queue import PriorityQueue
+# Carlier
+
+from operator import itemgetter
 import copy
+import timeit
+import time
 
 
-class Holder:
-    def __init__(self, vector_rpq):
-        self.vector_rpq = vector_rpq
+class PriorityQueue:
 
-    def __eq__(self, other):
-        return self.vector_rpq[2] == other.vector_rpq[2]
+    def __init__(self, key, rev, l):
+        self._key = key
+        self._rev = rev
+        if len(l) == 0:
+            self._list = []
+        else:
+            self._list = l.sort(key=key, reverse=rev)
 
-    def __lt__(self, other):
-        return self.vector_rpq[2] > other.vector_rpq[2]
+    def delete(self):
+        self._list.pop(len(self._list)-1)
 
-    def return_vector_rpq(self):
-        return self.vector_rpq
+    def add(self, val):
+        self._list.append(val)
+        self._list.sort(key=self._key, reverse=self._rev)
+        # print(f"key = {self._key}, list = {self._list}")
 
+    def display(self):
+        print(self._list)
+
+    def return_list(self):
+        return self._list
+
+    def is_empty(self):
+        if len(self._list) == 0:
+            return True
+        else:
+            return False
+
+    def get_element(self):
+        return self._list[len(self._list)-1]
+
+    def get_r(self, nb):
+        # print("r getted")
+        return self._list[len(self._list)-1][0]
 
 def read(nb):
     lst = []
-    with open('data\\Schrage' + str(nb) + '.DAT') as f:
+    with open('data\\SCHRAGE' + str(nb) + '.DAT') as f:
         f.readline() #pominięcie n
         for i in f:
             x, y, z = i.split()
             lst.append([int(x), int(y), int(z)])
     return sorted(lst, key=lambda i: i[0]) # sortowanie po r
 
+def get_anwser(nb):
+    with open('data\\CARLIER' + str(nb) + '.OUT') as f:
+        x = f.readline() #pominięcie n
+    return x
 
-def schrage(N):
-    N = sorted(N, key=lambda i: i[0])
-    Cmax = t = b = i = 0
-    G = PriorityQueue()
-    Pi = []
+def schrage(data):
+    t = 0
+    k = 0
+    b = 0 # indeks zadania z cmaxem w permutacji
+    N = PriorityQueue(itemgetter(0), True, [])
+    G = PriorityQueue(itemgetter(2), False, [])
 
-    while len(N) or not G.empty():
-        while len(N) and N[0][0] <= t:
-            G.put(Holder(N[0]))
-            del N[0]
-        if G.empty():
-            t = N[0][0]
-            continue
-        e = G.get().return_vector_rpq()
-        t += e[1]
-        if Cmax <= t + e[2]:
-            Cmax = t + e[2]
-            b = i
-        i += 1
-        Pi.append(e)
-    return Pi, Cmax, b
+    C_max = 0
+    pi = []
+
+    for i in range(0, len(data)):
+        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2])])
+
+    while G.is_empty() is False or N.is_empty() is False:  # 2
+        while N.is_empty() is False and int(N.get_r(0)) <= t:  # 3
+            e = N.get_element()
+            G.add(e)
+            N.delete()
+        if G.is_empty() is True:                                     # 5
+            t = int(N.get_r(0))                                    # 6
+        else:
+            e = G.get_element()
+            G.delete()
+            pi.append(e)
+            t += int(e[1])
+            if C_max <= int(t+int(e[2])):
+                C_max = int(t+int(e[2]))
+                b = k
+            k += 1
+            
 
 
-def pre_schrage(N):
-    N = sorted(N, key=lambda i: i[0])
-    Cmax = t = 0
-    G = PriorityQueue()
-    l = [0, 0, 9999999]  # q0~=inf
-    while len(N) or not G.empty():
-        while len(N) and N[0][0] <= t:
-            e = N[0]
-            G.put(Holder(N[0]))
-            del N[0]
-            if e[2] > l[2]:
-                l[1] = t - e[0]
+    return pi,C_max,b
+
+def schrage_div(data):
+    n = len(data)
+    t = 0                                           # total time
+    N = PriorityQueue(itemgetter(0), True, [])      # list of unordered tasks
+    G = PriorityQueue(itemgetter(2), False, [])     # list of ready to implementation tasks
+
+    C_max = 0
+    pi = []
+   
+
+    for i in range(0, n):
+        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2])])
+
+    onMachine=[0, 0, 999999]                    # current task initialization
+
+    while G.is_empty() is False or N.is_empty() is False:   # check if at least one OR lists isn't empty
+        while N.is_empty() is False and int(N.get_r(0)) <= t:  # check if N isn't empty AND availability time is less than total time
+            e = N.get_element()                                # ready task
+            G.add(e)
+            N.delete()
+            if e[2] > onMachine[2]:                            # check if time to complete ready task is higher than time on machine
+                onMachine[1] = t - e[0]
                 t = e[0]
-                if l[1] > 0:
-                    G.put(Holder(l))
-        if G.empty():
-            t = N[0][0]
-            continue
-        e = G.get().return_vector_rpq()
-        l = e
-        t += e[1]
-        Cmax = max(Cmax, t + e[2])
-    return Cmax
+                if onMachine[1] > 0:
+                    G.add(onMachine)
 
+        if G.is_empty() is True:                                     # 5
+            t = int(N.get_r(0))                                    # 6
+        else:
+            e = G.get_element()
+            G.delete()
+            onMachine = e
+            pi.append(e)
+            t += int(e[1])
+            C_max = max(C_max, int(t+int(e[2])))
+
+
+    return C_max
 
 def findA(lst, b, Cmax):
     sum = a = 0
@@ -85,13 +140,11 @@ def findA(lst, b, Cmax):
         a += 1
     return a
 
-
 def findC(lst, a, b):
     for i in range(b - 1, a - 1, -1):
         if lst[i][2] < lst[b][2]:
             return i
     return None
-
 
 def findRPQprim(lst, b, c):
     rprim = lst[c + 1][0]
@@ -107,47 +160,52 @@ def findRPQprim(lst, b, c):
 
     return rprim, pprim, qprim
 
-
-def calier(lst):
+def carlier(lst):
     lst, Cmax, b = schrage(copy.deepcopy(lst))
-    for i in lst:
-        a = findA(lst, b, Cmax)
-        c = findC(lst, a, b)
-        if not c:
-            return Cmax
+    a = findA(lst, b, Cmax)
+    c = findC(lst, a, b)
+    if not c:
+        return Cmax
 
-        rprim, pprim, qprim = findRPQprim(lst, b, c)
-        r_saved = lst[c][0]
-        lst[c][0] = max(lst[c][0], rprim + pprim)
-        LB = pre_schrage(copy.deepcopy(lst))
+    rprim, pprim, qprim = findRPQprim(lst, b, c)
+    r_saved = lst[c][0]
+    lst[c][0] = max(lst[c][0], rprim + pprim)
+    LB = schrage_div(copy.deepcopy(lst))
 
-        if LB < Cmax:
-            Cmax = min(Cmax, calier(lst))
+    if LB < Cmax:
+        Cmax = min(Cmax, carlier(lst))
 
-        lst[c][0] = r_saved
+    lst[c][0] = r_saved
 
-        q_saved = lst[c][2]
-        lst[c][2] = max(lst[c][2], qprim + pprim)
-        LB = pre_schrage(copy.deepcopy(lst))
+    q_saved = lst[c][2]
+    lst[c][2] = max(lst[c][2], qprim + pprim)
+    LB = schrage_div(copy.deepcopy(lst))
 
-        if LB < Cmax:
-            Cmax = min(Cmax, calier(lst))
+    if LB < Cmax:
+        Cmax = min(Cmax, carlier(lst))
 
-        lst[c][2] = q_saved
+    lst[c][2] = q_saved
 
     return Cmax
 
-
-# if __name__ == "__main__":
-#     # for i in range(1, 11):
-#     #     lst = read("SCHRAGE" + str(i) + ".DAT")
-#     #     val = calier(lst)
-#     #     print(val)
-#     lst = read("SCHRAGE" + str(10) + ".DAT")
-#     val = calier(lst)
-#     print(val)
+def test(start, end):
+    for i in range(start,end+1):
+        tasks = read(i)
+        start_time = time.time()
+        result = carlier(tasks)
+        execution_time = time.time() - start_time
+        print('Carlier: ' + str(result) +' | ' + 'Odp: '+ str(get_anwser(i)) + ' | Czas: ' + str(execution_time))
 
 
-xd = schrage(read(2))
 
-print(xd)
+test(1,10)
+    
+
+
+
+
+
+
+
+
+
